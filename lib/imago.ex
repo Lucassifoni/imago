8ede147b0d91fb619cd6af4c63b96ba1d45d1ea2
Lucas Sifoni <lucas@lucassifoni.info>
2019-05-27 19:28:53 +0200

    A bit more utilities

--- lib/imago.ex
+++ lib/imago.ex
@@ -1,7 +1,11 @@
 defmodule Imago do
   use Rustler, otp_app: :imago, crate: "imago"
 
-  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
-
   def read_pixels(_a), do: :erlang.nif_error(:nif_not_loaded)
+  def read_pixels_rgba(_a), do: :erlang.nif_error(:nif_not_loaded)
+  def read_pixels_rgb(_a), do: :erlang.nif_error(:nif_not_loaded)
+  def read_pixels_red(_a), do: :erlang.nif_error(:nif_not_loaded)
+  def read_pixels_green(_a), do: :erlang.nif_error(:nif_not_loaded)
+  def read_pixels_blue(_a), do: :erlang.nif_error(:nif_not_loaded)
+  def read_pixels_alpha(_a), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/imago/src/lib.rs
+++ native/imago/src/lib.rs
@@ -1,11 +1,8 @@
 extern crate image;
 #[macro_use] extern crate rustler;
-#[macro_use] extern crate rustler_codegen;
-#[macro_use] extern crate lazy_static;
 
-use rustler::{Env, Term, NifResult, Encoder, Error, Binary};
-use image::{GenericImageView, Pixel};
-use std::fs::File;
+use rustler::{Env, Term, NifResult, Encoder, Error};
+use image::{GenericImageView, DynamicImage};
 use std::path::Path;
 
 mod atoms {
@@ -19,27 +16,127 @@ mod atoms {
 
 rustler_export_nifs! {
     "Elixir.Imago",
-    [("add", 2, add), ("read_pixels", 1, read_pixels)],
+    [
+    ("read_pixels", 1, read_pixels),
+    ("read_pixels_rgba", 1, read_pixels_rgba),
+    ("read_pixels_rgb", 1, read_pixels_rgb),
+    ("read_pixels_red", 1, read_pixels_red),
+    ("read_pixels_green", 1, read_pixels_green),
+    ("read_pixels_blue", 1, read_pixels_blue),
+    ("read_pixels_alpha", 1, read_pixels_alpha),
+    ],
     None
 }
 
-fn read_pixels<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
-    let path: &'a str = args[0].decode()?;
+/* Utils */
+
+fn open_file_arg0<'a>(arg0: Term<'a>) -> Result<DynamicImage, Error> {
+    let path: &'a str = arg0.decode()?;
     if let Ok(f) = image::open(Path::new(path)) {
-        let mut rgbax: Vec<u8> = Vec::new();
-        for pixel in f.pixels() {
-            for chan in &(pixel.2.data) {
-                rgbax.push(*chan)
-            }
-        }
-        return Ok((atoms::ok(), rgbax).encode(env))
+        return Ok(f);
     }
     Err(rustler::Error::Atom("error"))
 }
 
-fn add<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
-    let num1: i64 = args[0].decode()?;
-    let num2: i64 = args[1].decode()?;
+/* Public */
 
-    Ok((atoms::ok(), num1 + num2).encode(env))
+fn read_pixels<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+    if let Ok(f) = open_file_arg0(args[0]) {
+        return Ok((atoms::ok(), read_pixels_r(f)).encode(env))
+    }
+    Err(rustler::Error::Atom("error"))
+}
+
+fn read_pixels_rgba<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+    read_pixels(env, args)
+}
+
+fn read_pixels_rgb<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+    if let Ok(f) = open_file_arg0(args[0]) {
+        return Ok((atoms::ok(), read_pixels_rgb_r(f)).encode(env))
+    }
+    Err(rustler::Error::Atom("error"))
+}
+
+fn read_pixels_red<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+    if let Ok(f) = open_file_arg0(args[0]) {
+        return Ok((atoms::ok(), read_pixels_red_r(f)).encode(env))
+    }
+    Err(rustler::Error::Atom("error"))
+}
+
+fn read_pixels_green<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+    if let Ok(f) = open_file_arg0(args[0]) {
+        return Ok((atoms::ok(), read_pixels_green_r(f)).encode(env))
+    }
+    Err(rustler::Error::Atom("error"))
+}
+
+fn read_pixels_blue<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+    if let Ok(f) = open_file_arg0(args[0]) {
+        return Ok((atoms::ok(), read_pixels_blue_r(f)).encode(env))
+    }
+    Err(rustler::Error::Atom("error"))
+}
+
+fn read_pixels_alpha<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+    if let Ok(f) = open_file_arg0(args[0]) {
+        return Ok((atoms::ok(), read_pixels_alpha_r(f)).encode(env))
+    }
+    Err(rustler::Error::Atom("error"))
+}
+
+/* Impls */
+
+fn read_pixels_rgb_r(i: DynamicImage) -> Vec<u8> {
+    let mut v = Vec::<u8>::new();
+    for pixel in i.pixels() {
+        v.push(pixel.2.data[0]);
+        v.push(pixel.2.data[1]);
+        v.push(pixel.2.data[2]);
+    }
+    v
+}
+
+fn read_pixels_red_r(i: DynamicImage) -> Vec<u8> {
+    let mut v = Vec::<u8>::new();
+    for pixel in i.pixels() {
+        v.push(pixel.2.data[0])
+    }
+    v
+}
+
+fn read_pixels_green_r(i: DynamicImage) -> Vec<u8> {
+    let mut v = Vec::<u8>::new();
+    for pixel in i.pixels() {
+        v.push(pixel.2.data[1])
+    }
+    v
+}
+
+fn read_pixels_blue_r(i: DynamicImage) -> Vec<u8> {
+    let mut v = Vec::<u8>::new();
+    for pixel in i.pixels() {
+        v.push(pixel.2.data[2])
+    }
+    v
+}
+
+fn read_pixels_alpha_r(i: DynamicImage) -> Vec<u8> {
+    let mut v = Vec::<u8>::new();
+    for pixel in i.pixels() {
+        v.push(pixel.2.data[3])
+    }
+    v
+}
+
+
+fn read_pixels_r(i: DynamicImage) -> Vec<u8> {
+    let mut v = Vec::<u8>::new();
+    for pixel in i.pixels() {
+        for chan in &(pixel.2.data) {
+            v.push(*chan)
+        }
+    }
+    v
 }
