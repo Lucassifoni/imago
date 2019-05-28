d5010a534b7634403d96a781a4f4de8661cecc06
Lucas Sifoni <lucas@lucassifoni.info>
2019-05-28 14:41:21 +0200

    Formatting

--- native/imago/src/lib.rs
+++ native/imago/src/lib.rs
@@ -1,16 +1,16 @@
 extern crate image;
-#[macro_use] extern crate rustler;
+#[macro_use]
+extern crate rustler;
 
-use rustler::{Env, Term, NifResult, Encoder, Error};
-use image::{GenericImageView, DynamicImage, FilterType};
 use std::path::Path;
+
+use image::{DynamicImage, FilterType, GenericImageView};
+use rustler::{Encoder, Env, Error, NifResult, Term};
 
 mod atoms {
     rustler_atoms! {
         atom ok;
         atom error;
-        //atom __true__ = "true";
-        //atom __false__ = "false";
     }
 }
 
@@ -46,7 +46,7 @@ fn open_file_arg0<'a>(arg0: Term<'a>) -> Result<DynamicImage, Error> {
 
 fn read_pixels<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     if let Ok(f) = open_file_arg0(args[0]) {
-        return Ok((atoms::ok(), read_pixels_r(f)).encode(env))
+        return Ok((atoms::ok(), read_pixels_r(f)).encode(env));
     }
     Err(rustler::Error::Atom("error"))
 }
@@ -57,35 +57,35 @@ fn read_pixels_rgba<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> 
 
 fn read_pixels_rgb<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     if let Ok(f) = open_file_arg0(args[0]) {
-        return Ok((atoms::ok(), read_pixels_rgb_r(f)).encode(env))
+        return Ok((atoms::ok(), read_pixels_rgb_r(f)).encode(env));
     }
     Err(rustler::Error::Atom("error"))
 }
 
 fn read_pixels_red<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     if let Ok(f) = open_file_arg0(args[0]) {
-        return Ok((atoms::ok(), read_pixels_red_r(f)).encode(env))
+        return Ok((atoms::ok(), read_pixels_red_r(f)).encode(env));
     }
     Err(rustler::Error::Atom("error"))
 }
 
 fn read_pixels_green<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     if let Ok(f) = open_file_arg0(args[0]) {
-        return Ok((atoms::ok(), read_pixels_green_r(f)).encode(env))
+        return Ok((atoms::ok(), read_pixels_green_r(f)).encode(env));
     }
     Err(rustler::Error::Atom("error"))
 }
 
 fn read_pixels_blue<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     if let Ok(f) = open_file_arg0(args[0]) {
-        return Ok((atoms::ok(), read_pixels_blue_r(f)).encode(env))
+        return Ok((atoms::ok(), read_pixels_blue_r(f)).encode(env));
     }
     Err(rustler::Error::Atom("error"))
 }
 
 fn read_pixels_alpha<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     if let Ok(f) = open_file_arg0(args[0]) {
-        return Ok((atoms::ok(), read_pixels_alpha_r(f)).encode(env))
+        return Ok((atoms::ok(), read_pixels_alpha_r(f)).encode(env));
     }
     Err(rustler::Error::Atom("error"))
 }
@@ -96,42 +96,16 @@ fn get_fingerprint<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
 
 fn get_fingerprint_4x4<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     if let Ok(f) = open_file_arg0(args[0]) {
-        return Ok((atoms::ok(), compute_fingerprint_4x4(f).to_vec()).encode(env))
+        return Ok((atoms::ok(), compute_fingerprint_4x4(f).to_vec()).encode(env));
     }
     Err(rustler::Error::Atom("error"))
 }
 
 fn get_fingerprint_8x8<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     if let Ok(f) = open_file_arg0(args[0]) {
-        return Ok((atoms::ok(), compute_fingerprint_8x8(f).to_vec()).encode(env))
+        return Ok((atoms::ok(), compute_fingerprint_8x8(f).to_vec()).encode(env));
     }
     Err(rustler::Error::Atom("error"))
-}
-
-fn compute_fingerprint_8x8(f: DynamicImage) -> Vec<u32> {
-    let g= f.resize_exact(32, 32, FilterType::Nearest);
-    let mut sign: [u32; 64] = [0; 64];
-    for pix in g.pixels() {
-        sign[index_2d_to_1d(pix.0 / 4, pix.1 / 4, 8) as usize] += pixel_luminance(pix.2.data[0], pix.2.data[1], pix.2.data[2]) as u32;
-    }
-    sign.iter().map(|a| *a / 16).collect()
-}
-
-fn compute_fingerprint_4x4(f: DynamicImage) -> Vec<u32> {
-    let g= f.resize_exact(32, 32, FilterType::Nearest);
-    let mut sign: [u32; 16] = [0; 16];
-    for pix in g.pixels() {
-        sign[index_2d_to_1d(pix.0 / 8, pix.1 / 8, 4) as usize] += pixel_luminance(pix.2.data[0], pix.2.data[1], pix.2.data[2]) as u32;
-    }
-    sign.iter().map(|a| *a / 64).collect()
-}
-
-fn index_2d_to_1d(x: u32, y: u32, rows: u32) -> u32 {
-    rows * y + x
-}
-
-fn pixel_luminance(r: u8, g: u8, b: u8) -> u8 {
-    ((r as f64 * 0.3) + (g as f64 * 0.59) + (b as f64 * 0.11)) as u8
 }
 
 /* Impls */
@@ -187,4 +161,30 @@ fn read_pixels_r(i: DynamicImage) -> Vec<u8> {
         }
     }
     v
+}
+
+fn compute_fingerprint_8x8(f: DynamicImage) -> Vec<u32> {
+    let g = f.resize_exact(32, 32, FilterType::Nearest);
+    let mut sign: [u32; 64] = [0; 64];
+    for pix in g.pixels() {
+        sign[index_2d_to_1d(pix.0 / 4, pix.1 / 4, 8) as usize] += pixel_luminance(pix.2.data[0], pix.2.data[1], pix.2.data[2]) as u32;
+    }
+    sign.iter().map(|a| *a / 16).collect()
+}
+
+fn compute_fingerprint_4x4(f: DynamicImage) -> Vec<u32> {
+    let g = f.resize_exact(32, 32, FilterType::Nearest);
+    let mut sign: [u32; 16] = [0; 16];
+    for pix in g.pixels() {
+        sign[index_2d_to_1d(pix.0 / 8, pix.1 / 8, 4) as usize] += pixel_luminance(pix.2.data[0], pix.2.data[1], pix.2.data[2]) as u32;
+    }
+    sign.iter().map(|a| *a / 64).collect()
+}
+
+fn index_2d_to_1d(x: u32, y: u32, rows: u32) -> u32 {
+    rows * y + x
+}
+
+fn pixel_luminance(r: u8, g: u8, b: u8) -> u8 {
+    ((r as f64 * 0.3) + (g as f64 * 0.59) + (b as f64 * 0.11)) as u8
 }
