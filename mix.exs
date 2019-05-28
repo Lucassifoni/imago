018048eb7762d5468e768dc6d85e3179fae1a573
Lucas Sifoni <lucas@lucassifoni.info>
2019-05-28 14:56:51 +0200

    Prepares for hex

--- mix.exs
+++ mix.exs
@@ -5,29 +5,35 @@ defmodule Imago.MixProject do
     [
       app: :imago,
       version: "0.1.0",
+      description: "Image manipulation at the pixel level, fingerprinting, [...]",
+      source_url: "https://github.com/Lucassifoni/imago",
+      package: [
+        name: "imago",
+        licenses: ["MIT"],
+        links: %{
+          "github" => "https://github.com/Lucassifoni/imago",
+        },
+        files:  ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE* native test_image.jpg license* CHANGELOG* changelog* src)
+      ],
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       compilers: [:rustler] ++ Mix.compilers(),
       rustler_crates: [imago: [
-        #mode: (if Mix.env() == :prod, do: :release, else: :debug)
+        # mode: (if Mix.env() == :prod, do: :release, else: :debug)
       ]]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:rustler, "~> 0.20.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
